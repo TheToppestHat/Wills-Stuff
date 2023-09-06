37675200fea8202dffae851c53c12bda9a6b101e
Joseph Whitmire <113870560+TheToppestHat@users.noreply.github.com>
2023-09-06 18:40:04 -0400

    Add files via upload

---
+++ Assignment1-CSCI-2910/Program.cs
@@ -0,0 +1,114 @@
+﻿/* Made by Joey Whitmire, 9/6/2023
+ * For Mr. Rochelle's 2910 Class
+ * Assignment 1
+ */
+
+using Assignment1_CSCI_2910;
+using System.Xml.Schema;
+
+char directorySep = Path.DirectorySeparatorChar;
+
+//Locates the file used in the assignment
+string projectFolder = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.ToString();
+string filePath = projectFolder + Path.DirectorySeparatorChar + "videogames.csv";
+List<VideoGames> myVideoGames = new List<VideoGames>();
+
+//Opens the reader for the file and makes the data viable
+using (StreamReader reader = new StreamReader(filePath))
+{
+    reader.ReadLine();
+
+    while (!reader.EndOfStream)
+    {
+        string currentgame = reader.ReadLine();
+        string[] internaldata = currentgame.Split(','); 
+        VideoGames nextgame = new VideoGames(internaldata[0], internaldata[1], Convert.ToInt32(internaldata[2]), internaldata[3], internaldata[4], Convert.ToDouble(internaldata[5]), Convert.ToDouble(internaldata[6]), Convert.ToDouble(internaldata[7]), Convert.ToDouble(internaldata[8]), Convert.ToDouble(internaldata[9]));
+        myVideoGames.Add(nextgame);
+    }
+
+    reader.Close();
+}
+
+//Sorts the list of games
+myVideoGames.Sort();
+
+
+//Creates the publisher list without user input
+var publish = myVideoGames.Where(p => p.Publisher == "Nintendo");
+foreach (var p in publish) Console.WriteLine(p);
+
+double totalGameCount = myVideoGames.Count();
+double totalpublishCount = publish.Count();
+
+Console.Write($"\n\nOut of {totalGameCount} games, {totalpublishCount} are developed by Nintendo, which is {Math.Round((totalpublishCount/totalGameCount)*100, 2)}%");
+
+
+
+//Creates the genre list without user input
+var genre = myVideoGames.Where(g => g.Genre == "Shooter");
+foreach (var g in genre) Console.WriteLine(g);
+
+double totalGenreCount =  genre.Count();
+Console.Write($"\n\nOut of {totalGameCount} games, {totalGenreCount} are developed as a Shooter, which is {Math.Round((totalGenreCount / totalGameCount) * 100, 2)}%");
+
+
+PublisherData(myVideoGames);
+
+//Method to use user input to find games
+static void PublisherData(List<VideoGames> myVideoGames)
+{
+    Console.Write("\n\n\nWhat is the publisher you want to use?  ");
+    var input = Console.ReadLine();
+
+    List<VideoGames> Publishlist = new List<VideoGames>();
+
+    foreach(var video in myVideoGames)
+    {
+        if (video.Publisher.ToUpper() == input.ToUpper())
+        {
+            Publishlist.Add(video);
+        }
+    }
+
+    double totalPublishCount = Publishlist.Count();
+    double totalCount = myVideoGames.Count();
+
+    foreach(var video in  Publishlist)
+    {
+        Console.WriteLine(video);
+    }
+
+    Console.Write($"\n\nOut of {totalCount} games, {totalPublishCount} are developed by {input}, which is {Math.Round(totalPublishCount/totalCount * 100, 2)}%");
+
+}
+
+GenreData(myVideoGames);
+
+
+//Method used to find user inputted genre and display results
+static void GenreData(List<VideoGames> myVideoGames)
+{
+    Console.Write("\n\n\nWhat is the genre of games you want to see?  ");
+    var input = Console.ReadLine();
+
+    List<VideoGames> Genrelist = new List<VideoGames>();
+
+    foreach (var video in myVideoGames)
+    {
+        if (video.Genre.ToUpper() == input.ToUpper())
+        {
+            Genrelist.Add(video);
+        }
+    }
+
+    double totalGenreCount = Genrelist.Count();
+    double totalCount = myVideoGames.Count();
+
+    foreach (var video in Genrelist)
+    {
+        Console.WriteLine(video);
+    }
+
+    Console.Write($"\n\nOut of {totalCount} games, {totalGenreCount} are developed by {input}, which is {Math.Round(totalGenreCount / totalCount * 100, 2)}%");
+
+}
---
+++ Assignment1-CSCI-2910/VideoGames.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1_CSCI_2910
+{
+    public class VideoGames : IComparable<VideoGames>
+    {
+        public string Name { get; set; }
+        public string Platform { get; set; }
+        public int Year { get; set; }
+        public string Genre { get; set; }
+        public string Publisher { get; set; }
+        public double NA_Sales { get; set; } 
+        public double EU_Sales { get; set; }
+        public double JP_Sales { get; set; }
+        public double Other_Sales { get; set; }
+        public double Global_Sales { get; set; }
+
+        public VideoGames(string name, string platform, int year, string genre, string publisher, double na_sales, double eu_sales, double jp_sales, double other_sales, double global_sales) 
+        {
+            Name = name;
+            Platform = platform;
+            Year = year;
+            Genre = genre;
+            Publisher = publisher;
+            NA_Sales = na_sales;
+            EU_Sales = eu_sales;
+            JP_Sales = jp_sales;
+            Other_Sales = other_sales;
+            Global_Sales = global_sales;
+        }
+
+        public VideoGames() { }
+
+        public int CompareTo(VideoGames otherGame)
+        {
+            if (otherGame == null)
+            {
+                return 1;
+            }
+            if(otherGame.Name != null)
+            {
+                return Comparer<string>.Default.Compare(Name, otherGame.Name);
+            }
+            else
+            {
+                throw new ArgumentException("This game does not exist");
+            }
+        }
+
+
+        //ToString for use in displaying a games information
+        public override string ToString()
+        {
+            string msg = $"\nName: {Name}";
+            msg += $"\nPublisher: {Publisher}";
+            msg += $"\nPlatform: {Platform}";
+            msg += $"\nGenre: {Genre}";
+            msg += $"\nYear: {Year}";
+            return msg;
+        }
+
+    }
+}
